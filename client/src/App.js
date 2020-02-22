c42e13b2467441046c97197f048808f6577da2f1
Craig <craigdanj@gmail.com>
2020-02-22 17:54:17 +0530

    tweaks to the readme

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import JurStatus from './contracts/JurStatus.json';
 import { DrizzleProvider} from 'drizzle-react';
 import { LoadingContainer, AccountData, ContractData, ContractForm } from 'drizzle-react-components';
+import Container from './Container';
 
 const drizzleOptions = {
 	contracts: [JurStatus]
@@ -14,12 +15,19 @@ function App() {
 				<div>
 					<h4>Account details:</h4>
 					<AccountData accountIndex={0} units={"ether"} precision={2} />
+					<p>-------------------------------------------------------</p>
+
+					{/* <h4>Status:</h4> */}
+					{/* <ContractData contract="JurStatus" method="status" methodArgs={[]}/> */}
+
+					{/* <h4>View 1st Status type:</h4>
+					<ContractData contract="JurStatus" method="statusTypes" methodArgs={[0]} /> */}
+
+					<h4>Add Jur Status Type:</h4>
+					<ContractForm contract="JurStatus" method="addStatusType" />
 
 					<h4>Status count:</h4>
 					<ContractData contract="JurStatus" method="statusCount" />
-
-					<h4>Status types:</h4>
-					<ContractForm contract="JurStatus" method="statusTypes" />
 
 					<h4>Add Jur Status:</h4>
 					<ContractForm contract="JurStatus" method="addJurStatus" />
@@ -27,8 +35,9 @@ function App() {
 					<h4>Change Jur Status State:</h4>
 					<ContractForm contract="JurStatus" method="changeState" />
 
-					<h4>Add Jur Status Type:</h4>
-					<ContractForm contract="JurStatus" method="addStatusType" />
+
+					<Container />
+
 				</div>
 			</LoadingContainer>
 		</DrizzleProvider>
